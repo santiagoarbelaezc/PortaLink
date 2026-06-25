1b44e59f6c0c79c951d8b9c4f6efd0426d64bee9
Santiago Arbelaez Contreras <santiago.arbelaezc@uqvirtual.edu.co>
2026-06-25 12:10:18 -0500

    instalando ng charts

--- portalink-frontend/src/app/app.config.ts
+++ portalink-frontend/src/app/app.config.ts
@@ -3,12 +3,14 @@ import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'top' })),
     provideAnimationsAsync(),
-    provideHttpClient()
+    provideHttpClient(),
+    provideCharts(withDefaultRegisterables())
   ]
 };
